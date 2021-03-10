97a2927f4f0bdb24f89cffc45d81ec6693ede3e1
cuscuz-itz666 <78763402+cuscuz-itz666@users.noreply.github.com>
2021-03-10 15:56:39 -0300

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,5 @@
 const config = {
-        botName: 'CAUSS_BOT',
-        ownerName: 'causs',
-        youtube: 'https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g',
-        instagram: 'https://instagram.com/animexx_png?igshid=e7zziiwn3d9y',
+        botName: 'BREDPIT_BOT',
+        ownerName: 'cuscuz,
+        instagram: 'http://instagram.com/cuscuz_com_queijo,
 }
